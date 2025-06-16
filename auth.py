0d627068a3754531dcfd799134e611cb547797cf
Unknown Haas <sharmasuhas450@gmail.com>
2025-06-16 18:10:52 +0530

    Implement Out-of-Band authorization flow for headless environments in auth.py

--- auth.py
+++ auth.py
@@ -3,8 +3,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-
-SCOPES=["https://www.googleapis.com/auth/calendar.readonly","https://www.googleapis.com/auth/calendar","https://www.googleapis.com/auth/gmail.readonly",'https://www.googleapis.com/auth/gmail.modify']
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/gmail.readonly", 'https://www.googleapis.com/auth/gmail.modify']
 
 def check_auth():
     creds = None
@@ -21,8 +20,27 @@ def check_auth():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(creds_file, SCOPES)
-            creds = flow.run_local_server(port=8088)
+            # Manually handle the Out-of-Band (OOB) flow for headless environments
+            flow = InstalledAppFlow.from_client_secrets_file(
+                creds_file,
+                SCOPES,
+                # This redirect_uri is crucial for the OOB flow
+                redirect_uri='urn:ietf:wg:oauth:2.0:oob'
+            )
+
+            # Generate the authorization URL
+            auth_url, _ = flow.authorization_url(prompt='consent')
+
+            print('Please visit this URL to authorize this application:')
+            print(auth_url)
+
+            # Get the authorization code from the user
+            code = input('Enter the authorization code: ')
+
+            # Exchange the code for a token
+            flow.fetch_token(code=code)
+            creds = flow.credentials
+
         with open(token_file, "w") as token:
             token.write(creds.to_json())
 
